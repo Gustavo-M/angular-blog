a3d6fa5a036f0ec340562923459aea2a1ec7baf1
Gustavo-M <5528005+Gustavo-M@users.noreply.github.com>
2024-01-20 00:38:55 -0300

    Criando conteúdos

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Foto Capim",
+        "description": "Capim Gordura",
+        "photoCover":"https://cdn.pixabay.com/photo/2017/04/10/01/28/mato-2217298_1280.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Foto Ponte Natureza",
+        "description": "Ponte na Natureza",
+        "photoCover":"https://cdn.pixabay.com/photo/2016/11/08/04/49/jungle-1807476_1280.jpg"
+    }
+]
